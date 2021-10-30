a053a7f1654389f21ed24fe1a929b59a5d071db5
dargon <simeilong@163.com>
2021-10-30 20:56:10 +0800

    改动3

---
+++ testdemo/src/test02.java
@@ -0,0 +1,7 @@
+public class test02 {
+    public static void main(String[] args) {
+        System.out.println("test02");
+        System.out.println("test02");
+        System.out.println("test02");
+    }
+}
